ec38996ab011ff7e3742985abbaaf249abddb967
jimdiosl <106568266+jimdiosl@users.noreply.github.com>
2025-05-10 22:43:53 -0500

    Add files via upload

---
+++ Code/python/seccion1/s1_script.py
@@ -0,0 +1,13 @@
+import ssl
+import urllib.request
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+
+ip = input("Enter IP to Lookup: ")
+
+url = f"https://ipapi.co/{ip.strip()}/json/"
+contents = urllib.request.urlopen(url, context=ctx).read()
+print(contents)
